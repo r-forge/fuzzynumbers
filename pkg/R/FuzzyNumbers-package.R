0241516927d51a15ffb975504e7a3c8553e95e3d
gagolews (Dr. ) <egzaltowany@gmail.com>
2012-06-09 21:23:13 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/fuzzynumbers@30 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/FuzzyNumbers-package.R
+++ pkg/R/FuzzyNumbers-package.R
@@ -10,51 +10,57 @@
 ##
 ## FuzzyNumbers is distributed in the hope that it will be useful,
 ## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 ## GNU Lesser General Public License for more details.
 ##
 ## You should have received a copy of the GNU Lesser General Public License
 ## along with FuzzyNumbers. If not, see <http://www.gnu.org/licenses/>.
 
 
-#' FuzzyNumbers is an open source (LGPL 3) package for R.
-#'     It provides S4 classes and methods to deal with Fuzzy Numbers.
-#'     It may be used both by the practitioners (computations of many
-#'     operations on fuzzy numbers, approximation by trapezoidal and piecewise
-#'     linear fuzzy numbers, etc.) but also by the researchers
-#'     in fuzzy numbers theory.
+#' \code{FuzzyNumbers} is an open source (LGPL 3) package for R.
+#' It provides S4 classes and methods to deal with Fuzzy Numbers
+#' and allows for computations of arithmetic operations on FNs,
+#' approximation by trapezoidal and piecewise linear, etc.
+#' The package may be used by the practitioners as well as by the researchers
+#' in fuzzy numbers theory (e.g. for preparing figures,
+#' generating numerical examples, or testing new algorithms).
 #'
 #' Fuzzy set theory lets us effectively and quite intuitively represent
 #' imprecise or vague information. Fuzzy numbers, which form a particular
 #' subclass of fuzzy sets of the real line, play a significant role
-#' in many important theoretical as well as practical considerations
-#' since we often describe our knowledge about objects
+#' in many important both theoretical and practical considerations.
+#' This is because we often describe our knowledge about objects
 #' through numbers, e.g. "I'm about 180 cm tall"
 #' or "The rocket was launched between 2 and 3 p.m.".
 #'
 #' For the formal definition of a fuzzy number
 #' please refer to the \code{\link{FuzzyNumber}} class man page.
 #' Note that this package also deals with particular types
-#' of fuzzy numbers like Trapezoidal and Piecewise Linear
-#' Fuzzy Numbers (see \code{\link{TrapezoidalFuzzyNumber}}
-#' and [TO DO]).
+#' of fuzzy numbers like trapezoidal and piecewise linear FNs
+#' (see \code{\link{TrapezoidalFuzzyNumber}}
+#' and \code{\link{PiecewiseLinearFuzzyNumber}}).
 #'
-#' The package aims to provide the following functionality.
+#' The package aims to provide the following functionality:
 #' \enumerate{
-#'    \item Operations on Fuzzy Numbers [TO DO],
-#'    \item Defuzzification and Approximation of Fuzzy Numbers by Triangular
-#'      and Piecewise Linear Fuzzy Numbers (see \code{\link{expectedValue}},
-#'      \code{\link{weightedExpectedValue}}, \code{\link{value}}, \code{\link{trapezoidalApproximation}}),
-#'    \item Visualization (see \code{\link{plot}}),
-#'    \item Aggregation of Fuzzy Numbers [TO DO],
-#'    \item Ranking of Ruzzy Numbers [TO DO],
+#'    \item Defuzzification and Approximation by Triangular
+#'      and Piecewise Linear FNs (see e.g. \code{\link{expectedValue}},
+#'      \code{\link{value}}, \code{\link{trapezoidalApproximation}},
+#'      \code{\link{piecewiseLinearApproximation}}),
+#'    \item Visualization of FNs (see \code{\link{plot}}),
+#'    \item Operations on FNs [TO DO],
+#'    \item Aggregation of FNs [TO DO],
+#'    \item Ranking of FNs [TO DO],
 #'    \item \dots
 #' }
 #'
 #' Please feel free to send any comments and feature requests to the author
-#' (see also \url{http://www.ibspan.waw.pl/~gagolews}).
+#' (see his homepage at \url{http://www.ibspan.waw.pl/~gagolews}).
 #'
-#' For a complete list of classes and methods, call \code{library(help="FuzzyNumbers")}.
+#' For a complete list of classes and methods
+#' call \code{library(help="FuzzyNumbers")}.
+#' Moreover, you will surely be interested in a step-by-step guide to the package
+#' features which is available at
+#' \url{http://www.ibspan.waw.pl/~gagolews/FuzzyNumbers/doc/FuzzyNumbers-Tutorial.pdf}.
 #' \cr\cr
 #'
 #' \bold{Keywords}: Fuzzy Numbers, Fuzzy Sets, Shadowed Sets,
@@ -66,11 +72,18 @@
 #' @title Tools to deal with fuzzy numbers in R
 #' @author Marek Gagolewski \email{gagolews@@ibspan.waw.pl}
 #' @references
-#' FuzzyNumbers Package Homepage, \url{http://www.ibspan.waw.pl/~gagolews/FuzzyNumbers/}.\cr
-#' Dubois D., Prade H. (1978), Operations on fuzzy numbers, \emph{Int. J. Syst. Sci.} 9, pp. 613-626.\cr
+#' FuzzyNumbers \code{FuzzyNumbers} Homepage, \url{http://www.ibspan.waw.pl/~gagolews/FuzzyNumbers/}.\cr
 #' Ban A.I. (2008), Approximation of fuzzy numbers by trapezoidal fuzzy numbers
 #' preserving the expected interval, Fuzzy Sets and Systems 159, pp. 1327-1344.\cr
+#' Chanas S. (2001), On the interval approximation of a fuzzy number,
+#' Fuzzy Sets and Systems 122, pp. 353-356.\cr
+#' Delgado M., Vila M.A., Voxman W. (1998), On a canonical representation of a fuzzy number,
+#' Fuzzy Sets and Systems 93, pp. 125-135.\cr
+#' Dubois D., Prade H. (1978), Operations on fuzzy numbers, Int. J. Syst. Sci. 9, pp. 613-626.\cr
+#' Dubois D., Prade H. (1987), The mean value of a fuzzy number, Fuzzy Sets and Systems 24, pp. 279-300.\cr
 #' Grzegorzewski P. (2010), Algorithms for trapezoidal approximations of fuzzy numbers
+#' Gagolewski M. (2012), Guide to the \code{FuzzyNumbers} package for R,
+#' \url{http://www.ibspan.waw.pl/~gagolews/FuzzyNumbers/doc/FuzzyNumbers-Tutorial.pdf}, 2012.
 #' preserving the expected interval, in: Bouchon-Meunier B. et al (Eds.),
 #' Foundations of Reasoning Under Uncertainty, Springer, pp. 85-98.\cr
 #' Grzegorzewski P, Pasternak-Winiarska K. (2011), Trapezoidal approximations of fuzzy numbers
@@ -83,5 +96,5 @@ NA
 
 .onLoad <- function(lib, pkg)
 {
-   packageStartupMessage("FuzzyNumbers loaded (http://www.ibspan.waw.pl/~gagolews/FuzzyNumbers/).");
+   packageStartupMessage("FuzzyNumbers loaded (www.ibspan.waw.pl/~gagolews/FuzzyNumbers/).");
 }
